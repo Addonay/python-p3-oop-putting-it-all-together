03a9a41b0a890c2a02500c4666ae814ad555d4bf
Addonay <addonay.osoro@student.moringaschool.com>
2023-08-28 10:03:30 +0300

    solved the lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
+
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "Used"
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+            if value < 0:
+                print("Size cannot be negative")
+                self._size = 0
